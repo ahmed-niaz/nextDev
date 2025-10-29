d613a42ce3b56077cafa647d4195e3bbc3c57cc4
ahmed-niaz <niazahmed.an@gmail.com>
2025-10-30 00:48:23 +0600

    next dev with nextjs 16

---
+++ components/Navbar.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <header>
+      <nav>
+        <Link href="/" className="logo">
+          <Image src="/icons/logo.png" alt="logo" width={24} height={24} />
+          <p>Next Dev</p>
+        </Link>
+
+        <ul>
+          <Link href="/">home</Link>
+          <Link href="/">events</Link>
+          <Link href="/">create events</Link>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
